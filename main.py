167b247c85ea363401e533874e5ec618bd136145
okoyom-1 <58003716+okoyom-1@users.noreply.github.com>
2023-10-02 23:48:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+import random
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+    
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 1000))
+
+class RandomApp(App):
+
+    def build(self):
+        return MyRoot()
+
+randomApp = RandomApp()
+randomApp.run()
